603791781ebfdba9415f70b59dace16c1376b5d7
Christina <cecurrier92@gmail.com>
2024-01-16 10:31:33 +0900

    integrated markdown-it

--- markdown-editor/src/components/MarkdownEditor.jsx
+++ markdown-editor/src/components/MarkdownEditor.jsx
@@ -1,57 +1,62 @@
-import React, { useState, useContext } from 'react';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
-import { Row } from 'react-bootstrap';
+import React, { useState, useContext, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import { Row } from "react-bootstrap";
 import Badge from 'react-bootstrap/Badge';
 import { ThemeContext } from "./../contexts/ThemeContext";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../css/App.css';
+import MarkdownIt from "markdown-it";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/App.css";
+
 
 
 function MarkdownEditor() {
-    const { colClass, setColClass } = useContext(ThemeContext);
 
-  // update the state
-  const topDown = (isChecked) => {
+const [inputMarkdown, setInputMarkdown] = useState("");
+const [outputHtml, setOutputHtml] = useState("");
+
+// create a state
+const { colClass, setColClass } = useContext(ThemeContext);
+
+// update the state
+const topDown = (isChecked) => {
     return isChecked ? setColClass(`col-12`) : setColClass(`col-6`);
-  };
+};
 
-  // Task 3: Add Layout code below
+useEffect(() => {
+    const md = new MarkdownIt();
+    const parsedValue = md.render(inputMarkdown);
+    setOutputHtml(parsedValue);
+}, [inputMarkdown]);
 
-  return (
+// Task 3: Add Layout code below
+
+return (
     <div>
-      <Form className="d-flex justify-content-end gap-2 mb-3">
-        <label htmlFor="">top-down</label>
-        <Form.Check
-          type="switch"
-          id="custom-switch"
-          onClick={(e) => topDown(e.target.checked)}
-        />
-      </Form>
-      <Row className="mb-5">
-        <Col className={`mb-4 ${colClass}`}>
-          <Badge
-            style={{ fontSize: 25 }}
-            className="bg-success text-white active text-nowrap fw-bold"
-          >
-            Enter Markdown
-          </Badge>
+        <Form className="d-flex justify-content-end gap-2 mb-3">
+            <label htmlFor="">top-down</label>
+            <Form.Check
+                type="switch"
+                id="custom-switch"
+                onClick={(e) => topDown(e.target.checked)}
+            />
+        </Form>
+        <Row className="mb-5">
 
-          <textarea className="markdown-editor form-control rounded-bottom "></textarea>
-        </Col>
-        <Col className={`mb-4 ${colClass}`}>
-          <Badge
-            style={{ fontSize: 25 }}
-            className="bg-success text-white active text-nowrap fw-bold"
-          >
-            Preview
-          </Badge>
+            <Col className={`mb-4 ${colClass}`}>
+              <Badge style={{fontSize: 25}} className="bg-success text-white active text-nowrap fw-bold">Enter Markdown</Badge>
 
-          <div className="markdown-editor form-control rounded-bottom"></div>
-        </Col>
-      </Row>
+                <textarea value={inputMarkdown}
+                    onChange={(e) => setInputMarkdown(e.target.value)} className="markdown-editor form-control rounded-bottom "></textarea>
+            </Col>
+            <Col className={`mb-4 ${colClass}`}>
+              <Badge style={{fontSize: 25}} className="bg-success text-white active text-nowrap fw-bold">Preview</Badge>
+
+                <div dangerouslySetInnerHTML={{ __html: outputHtml }} className="markdown-editor form-control rounded-bottom"></div>
+            </Col>
+        </Row>
     </div>
-  );
+);
 }
 
 export default MarkdownEditor;
